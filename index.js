a7629265219cafaee18337478120b6c6478c0f39
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-05-10 19:53:03 +0100

    master

--- index.js
+++ index.js
@@ -6,8 +6,24 @@
 // let count = firstBatch + secondBatch
 // console.log(count)
 
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked (log it out)
+// change the count-el in the HTML to reflect the new count
+
+/* const soundBtn = document.querySelector('increment.btn')
+let myAudio = document.querySelector('#audio');
+soundBtn.addEventListener('click',()=>{myAudio.play();});
+fuction incrementBtn() {
+
+} */
+
+let count = 0
 function increment() {
-
-console.log("The button is clicked")
-
+    
+    count = count + 1
+     
+    
 }
+increment()
